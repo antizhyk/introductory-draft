37b448169f21172bea5a972f070142cc863492cf
antizhyk <01antizyk.it@gmail.com>
2021-02-11 16:08:05 +0200

    Создал страницу входа

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Project</title>
+	<link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
-
+	<div class="wrapper">
+		<div class="entry">
+			<h1 class="entry__title">Добро пожаловать!</h1>
+			<form method="GET" action="" class="entry__form form">
+				<div class="entry__inputs">
+					<input type="text" name="login" placeholder="Введите email" class="entry__login entry__input">
+					<input type="password" name="password" placeholder="Введите пароль" class="entry__password entry__input">
+				</div>
+				<div class="entry__buttons">
+					<a href="" class="entry__register entry__button">Зарегистрироваться</a>
+					<button class="entry__submit entry__button" type="submit">Войти</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 
 </html>
