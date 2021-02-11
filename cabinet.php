fca4780432c1df7b142b18cf5399b95c711e25a2
antizhyk <01antizyk.it@gmail.com>
2021-02-11 22:30:31 +0200

    Завершил создание личного кабинета

--- cabinet.php
+++ cabinet.php
@@ -14,7 +14,26 @@
 		<div class="sub-wrapper sub-wrapper__cabinet">
 			<div class="cabinet">
 				<div class="cabinet__photo">
-
+					<div class="cabinet__avatar">
+						<img src="./img/ogromnyy-kosmicheskiy-korabl-budushhego.jpg" alt="" class="cabinet__img">
+					</div>
+				</div>
+				<div class="cabinet__info">
+					<div class="cabinet__name">
+						Матвиенко Александр Олегович
+					</div>
+					<div class="cabinet__contacts">
+						<div class="cabinet__title-contacts">Контакты:</div>
+						<ul class="cabinet__list">
+							<li>
+								<a href="tel:+380997103208" class="cabinet__telephon cabinet__link">+380997103208</a>
+							</li>
+							<li>
+								<a href="mailto:sashamatvienk0@gmail.com" class="cabinet__email cabinet__link">sashamatvienk0@gmail.com</a>
+							</li>
+						</ul>
+					</div>
+					<a href="" class="cabinet__upload">Изменить данные</a>
 				</div>
 			</div>
 		</div>
